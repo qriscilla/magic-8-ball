5325f6afa48b774c99bb9843bede9ff0b591634d
qriscilla <pri.whang@gmail.com>
2020-02-09 17:02:32 -0500

    Updates

---
+++ precache-manifest.9be7da2d3fef97ecc5ab670902334d7e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72f8ef72538cd176b828cb26b5a5fb73",
+    "url": "/magic-8-ball/index.html"
+  },
+  {
+    "revision": "1fbca78da395f165e0ec",
+    "url": "/magic-8-ball/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "26b9c063a96fd5ad14b0",
+    "url": "/magic-8-ball/static/css/main.1ecfdaed.chunk.css"
+  },
+  {
+    "revision": "1fbca78da395f165e0ec",
+    "url": "/magic-8-ball/static/js/2.f9348276.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/magic-8-ball/static/js/2.f9348276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26b9c063a96fd5ad14b0",
+    "url": "/magic-8-ball/static/js/main.b5816f38.chunk.js"
+  },
+  {
+    "revision": "7990284a7449e308fde1",
+    "url": "/magic-8-ball/static/js/runtime-main.3591f471.js"
+  },
+  {
+    "revision": "6cad85d6cee9687fe107209058293adc",
+    "url": "/magic-8-ball/static/media/EightBall.6cad85d6.png"
+  }
+]);
