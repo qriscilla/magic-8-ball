cfccd37e1e2d1d96c10024ad107bea8af7e7254e
qriscilla <pri.whang@gmail.com>
2021-07-13 22:26:43 -0500

    Updates

--- precache-manifest.78616d1b565e32a5b37e552b1d05b9fb.js
+++ precache-manifest.78616d1b565e32a5b37e552b1d05b9fb.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e9e8f3377c0a86065f10fa083fb1d395",
+    "revision": "3de201be6fc8079dcf876dbf419e9f77",
     "url": "/magic-8-ball/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/magic-8-ball/static/css/2.47e06e2e.chunk.css"
   },
   {
-    "revision": "cd71526d3e62ea602dc5",
-    "url": "/magic-8-ball/static/css/main.bf7a3564.chunk.css"
+    "revision": "afe0b193939c4ac96181",
+    "url": "/magic-8-ball/static/css/main.faf4babe.chunk.css"
   },
   {
     "revision": "460782fd7ae64cbc4b4f",
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/magic-8-ball/static/js/2.b1eb0706.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "cd71526d3e62ea602dc5",
+    "revision": "afe0b193939c4ac96181",
     "url": "/magic-8-ball/static/js/main.72c4ffa2.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/magic-8-ball/precache-manifest.5161d681163fd480019892c44c85e454.js"
+  "/magic-8-ball/precache-manifest.78616d1b565e32a5b37e552b1d05b9fb.js"
 );
 
 self.addEventListener('message', (event) => {
